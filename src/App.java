ef9f185724047ee338b165a7119d17282e4addc7
Leo <leonardo.chuquilin@usil.pe>
2023-08-29 20:28:23 -0500

    Commit devlop

--- src/App.java
+++ src/App.java
@@ -6,5 +6,6 @@ public class App {
         Triangle t = new Triangle(new Point(0,0), new Point(6,0), new Point(3,5));
         t.setName("Equilatero");
         System.out.println(t.toString());
+        
     }
 }
